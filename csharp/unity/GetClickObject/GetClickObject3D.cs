ec589540d185b9525e4abc45d010c8fd00e63ea8
kota-shiokara <ikanoshiokara.fun@gmail.com>
2021-01-15 04:29:35 +0900

    create GetClickObject.cs

---
+++ csharp/unity/GetClickObject/GetClickObject3D.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetClickObject3D : MonoBehaviour
+{
+    GameObject clickedGameObject;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            clickedGameObject = null;
+
+            //Rayを視点からマウスの位置に飛ばす
+            Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(myRay, out hit, Mathf.Infinity))
+            {
+                //Rayに当たったgameobjectを参照
+                clickedGameObject = hit.collider.gameObject;
+            }
+            if(clickedGameObject != null)
+            {
+
+            }
+            Debug.Log(clickedGameObject.name);
+        }
+    }
+}
